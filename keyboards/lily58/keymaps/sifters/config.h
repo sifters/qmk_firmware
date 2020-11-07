afe14f11c8d850d76afa4bb45d6ae1852e2ac7ca
Eugene Rinaldi <eugene@sifters.net>
2020-11-07 08:18:02 -0600

    add mouse keys; movement to raised layer

--- keyboards/lily58/keymaps/sifters/config.h
+++ keyboards/lily58/keymaps/sifters/config.h
@@ -50,3 +50,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_SLEEP
 */
+
+
+// Mouse Settings
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
