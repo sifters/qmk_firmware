2ca1c553ca40ffee8219e5eeeb424f4db342e8d3
Eugene Rinaldi <eugene@sifters.net>
2021-01-11 06:55:22 -0600

    add colemak and game layers

--- keyboards/lily58/keymaps/sifters/keymap.c
+++ keyboards/lily58/keymaps/sifters/keymap.c
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_GAME] = LAYOUT( \
   KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,   \
-  T_LCTRL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
+  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT_BSLS,  \
                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,  TL_RSE,   KC_ENT,  KC_BSPC, KC_DEL \
 ),
@@ -230,17 +230,22 @@ void oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
     //oled_write_ln(read_layer_state(), false);
+     oled_write_P(PSTR("Layout: "), false);
+     switch (biton32(default_layer_state)) {
+         case _QWERTY:
+             oled_write_P(PSTR("QWERTY\n"), false);
+             break;
+         case _COLEMAK:
+             oled_write_P(PSTR("Colemak\n"), false);
+             break;
+         case _GAME:
+             oled_write_P(PSTR("Game\n"), false);
+             break;
+         default:
+             oled_write_P(PSTR("Undefined "), false);
+        } 
     oled_write_P(PSTR("Layer: "), false);
     switch(get_highest_layer(layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-        case _COLEMAK:
-            oled_write_P(PSTR("Colemak\n"), false);
-            break;
-        case _GAME:
-            oled_write_P(PSTR("Game\n"), false);
-            break;
         case _RAISE:
             oled_write_P(PSTR("Raise\n"), false);
             break;
@@ -254,14 +259,14 @@ void oled_task_user(void) {
             oled_write_P(PSTR("Number Pad\n"), false);
             break;
         default:
-            oled_write_P(PSTR("Undefined\n"), false);
+            oled_write_P(PSTR("Default\n"), false);
     }       
     led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ?       PSTR("[NUM] ")   : PSTR("      "), false);
-    oled_write_P(led_state.caps_lock ?      PSTR("[CAP] ")   : PSTR("      "), false);
-    oled_write_P(led_state.scroll_lock ?    PSTR("[SCR] \n") : PSTR("      \n"), false);
-    oled_write_ln(read_keylog(), 
-    oled_write_ln(read_keylogs(), false);
+    oled_write_P(led_state.num_lock ?       PSTR("NUM ")   : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ?      PSTR("CAP ")   : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ?    PSTR("SCR \n") : PSTR("    \n"), false);
+    oled_write_ln(read_keylog(), false); 
+    //oled_write_ln(read_keylogs(), false);
 
     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
