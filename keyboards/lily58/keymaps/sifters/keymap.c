64d2173f4cd610e3e5855e857f5f2fd1e7f157db
Eugene Rinaldi <eugene@sifters.net>
2021-05-10 04:38:13 -0500

    add shift to numpad

--- keyboards/lily58/keymaps/sifters/keymap.c
+++ keyboards/lily58/keymaps/sifters/keymap.c
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | LCTRL|      | Left | Down | Right|      |-------.    ,-------| PGDN |  P4  |  P5  |  P6  |  .   | KPENT|
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |  P1  |  P2  |  P3  |  =   |      |
+ * | LSFT |      |      |      |      |      |-------|    |-------|      |  P1  |  P2  |  P3  |  =   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Space \  |  P0  |  .   |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -198,7 +198,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_PSLS, KC_PAST, KC_PMNS, KC_BSPC, \
   _______, XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX,                   KC_PGUP, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_DEL,  \
   T_LCTRL, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                   KC_PGDN, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, KC_PENT, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, XXXXXXX, \
+  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, XXXXXXX, \
                              _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, _______ \
   )
 };
